8a00596bdcf33bd588b094e1304e4b649d269e87
EDIVAN DA SILVA JUNIOR <edivanjr13@gmail.com>
2022-12-10 18:55:55 -0300

    Menu aprimorado

--- menu003/sideBarMenu.js
+++ menu003/sideBarMenu.js
@@ -2,8 +2,9 @@ let btnOpenMenu = document.getElementById('iconBurger')
 let btnCloseMenu = document.getElementById('closeMenu')
 let sideBarMenu = document.getElementById('sideBar')
 
+
 btnOpenMenu.onclick = function() {
-    if(sideBarMenu.style.left == '') {
+    if(sideBarMenu.style.left != '0') {
         sideBarMenu.style.left = '0'
     }
 }
@@ -13,3 +14,10 @@ btnCloseMenu.onclick = function() {
         sideBarMenu.style.left = '-250px'
     }
 }
+
+window.onscroll = function() {
+    if(sideBarMenu.style.left == '0px') {
+        sideBarMenu.style.left = '-250px'
+    }
+}
+
