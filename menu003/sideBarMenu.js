88ba5adfbdd6796828c85e4fd2ca2d71995ab4db
EDIVAN DA SILVA JUNIOR <edivanjr13@gmail.com>
2022-12-10 11:29:51 -0300

    Abrir e fechar menu com alguns problemas

---
+++ menu003/sideBarMenu.js
@@ -0,0 +1,15 @@
+let btnOpenMenu = document.getElementById('iconBurger')
+let btnCloseMenu = document.getElementById('closeMenu')
+let sideBarMenu = document.getElementById('sideBar')
+
+btnOpenMenu.onclick = function() {
+    if(sideBarMenu.style.left == '') {
+        sideBarMenu.style.left = '0'
+    }
+}
+
+btnCloseMenu.onclick = function() {
+    if(sideBarMenu.style.left == '0px') {
+        sideBarMenu.style.left = '-250px'
+    }
+}
