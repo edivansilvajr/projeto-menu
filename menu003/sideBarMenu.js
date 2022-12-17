fa9d2db502fc66cd634c38d0dc9c809e7b59f706
EDIVAN DA SILVA JUNIOR <edivanjr13@gmail.com>
2022-12-17 00:49:30 -0300

    Bug resolvido, falta remover códigos desnecessários

--- menu003/sideBarMenu.js
+++ menu003/sideBarMenu.js
@@ -3,41 +3,73 @@ let btnCloseMenu = document.getElementById('closeMenu')
 let sideBarMenu = document.getElementById('sideBar')
 
 
-btnOpenMenu.onclick = function() {
-    if(sideBarMenu.style.left != '0') {
+// btnOpenMenu.onclick = function() {
+//     if(sideBarMenu.style.left != '0') {
+//         sideBarMenu.style.left = '0'
+//     }
+// }
+
+// btnCloseMenu.onclick = function() {
+//     if(sideBarMenu.style.left == '0px') {
+//         sideBarMenu.style.left = '-250px'
+//         document.body.style.overflow = 'auto'
+//     }
+// }
+
+// sideBarMenu.onmouseover = function () {
+//     document.body.style.overflow = 'hidden'
+// }
+
+
+// sideBarMenu.onmouseout = function () {
+//     document.body.style.overflow = 'auto'
+// }
+
+// window.onscroll = function () {
+//     sideBarMenu.style.left = '-250px'
+// }
+
+// touch events ---------------------------
+btnOpenMenu.onclick = function () {
+    if (sideBarMenu.style.left != '0') {
         sideBarMenu.style.left = '0'
     }
 }
 
-btnCloseMenu.onclick = function() {
-    if(sideBarMenu.style.left == '0px') {
-        sideBarMenu.style.left = '-250px'
-        document.body.style.overflow = 'auto'
+btnCloseMenu.ontouchend = function () {
+    if (sideBarMenu.style.left == '0px') {
+        sideBarMenu.style.left = '-250px' 
     }
 }
 
-sideBarMenu.onmouseover = function () {
+sideBarMenu.ontouchmove = function (e) {
+    e.stopPropagation()
     document.body.style.overflow = 'hidden'
 }
 
-
-sideBarMenu.onmouseout = function () {
-    document.body.style.overflow = 'auto'
+sideBarMenu.ontouchstart = function (e) {
+    e.stopPropagation()
+    document.body.style.overflow = 'hidden'
 }
 
-window.onscroll = function () {
+sideBarMenu.ontouchend = function (e) {
+    e.stopPropagation()
+    document.body.style.overflow = 'hidden'
+}
+
+sideBarMenu.ontouchcancel = function (e) {
+    e.stopPropagation()
+    document.body.style.overflow = 'hidden'
+}
+
+document.body.ontouchmove = function () {
     sideBarMenu.style.left = '-250px'
-}
-
-// touch events ---------------------------
-sideBarMenu.ontouchmove = function () {
-    document.body.style.overflow = 'hidden'
-}
-
-document.body.onmousemove = function () {
-    document.body.style.overflow = 'auto'
 }
 
 document.body.ontouchstart = function () {
     document.body.style.overflow = 'auto'
+}
+
+document.body.ontouchend = function () {
+    sideBarMenu.style.left = '-250px'
 }
